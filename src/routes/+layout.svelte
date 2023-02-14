bcc7ec7f48fb6562e71044be626b50d9bf22fa54
Claudio Rivera <me@claudiorivera.com>
2023-02-14 08:09:44 -0600

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,5 +23,23 @@
 </nav>
 
 <main class="max-w-md mx-auto">
+	{#if $page.data.session}
+		{#if $page.data.session.user?.image}
+			<span style="background-image: url('{$page.data.session.user.image}')" />
+		{/if}
+		<span>
+			<small>Signed in as</small><br />
+			<strong
+				>{$page.data.session.user?.email ??
+					$page.data.session.user?.name}</strong
+			>
+		</span>
+		<a href="/auth/signout" class="button" data-sveltekit-preload-data="off"
+			>Sign out</a
+		>
+	{:else}
+		<span>You are not signed in</span>
+		<a href="/auth/signin" data-sveltekit-preload-data="off">Sign in</a>
+	{/if}
 	<slot />
 </main>
