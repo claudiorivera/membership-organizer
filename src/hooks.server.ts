bc42af91b40be2fa0a407b628dab84587a9f41d3
Claudio Rivera <me@claudiorivera.com>
2023-02-22 20:09:25 -0600

    🐛 Add secret config key

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,6 +2,7 @@ import {
 	AUTH0_CLIENT_ID,
 	AUTH0_CLIENT_SECRET,
 	AUTH0_ISSUER,
+	AUTH_SECRET,
 } from "$env/static/private";
 import Auth0 from "@auth/core/providers/auth0";
 import { SvelteKitAuth } from "@auth/sveltekit";
@@ -15,4 +16,5 @@ export const handle = SvelteKitAuth({
 			issuer: AUTH0_ISSUER,
 		}),
 	],
+	secret: AUTH_SECRET,
 });
